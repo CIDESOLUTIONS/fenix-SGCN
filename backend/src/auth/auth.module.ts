ddf9f8adffacbc637f56ef8fa7e7594d4ff79ac2
MarioCifuentes <meciza@gmail.com>
2025-09-24 08:32:17 -0500

    Correciones usuario en dashboard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategy';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
